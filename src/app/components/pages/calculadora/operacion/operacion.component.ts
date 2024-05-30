03800a94644570b53515e660fb810542969fb9a7
stemenviewnext <esteven.menendez@viewnext.com>
2024-05-30 13:26:13 +0200

    fix: Calculadora Finalizada

--- src/app/components/pages/calculadora/operacion/operacion.component.ts
+++ src/app/components/pages/calculadora/operacion/operacion.component.ts
@@ -8,33 +8,17 @@ import { evaluate } from 'mathjs';
   templateUrl: './operacion.component.html',
   styleUrls: ['./operacion.component.css']
 })
-export class OperacionComponent{
+export class OperacionComponent {
 
-
-operadoresUnicos(){
-
-}
-
-revisarOperadores() {
-
-   if (['+', '-', '*', '/'].includes( this.currentInput[this.currentInput.length-1] )
-    && ['+', '-', '*', '/'].includes( this.currentInput[this.currentInput.length-2] )){
-
-      this.currentInput = this.currentInput.substring(0,this.currentInput.length-2) + this.currentInput[this.currentInput.length-1];
-
-  }
-  console.log('Hola: ' +this.currentInput[this.currentInput.length-1] )
-
-
-}
-
+  private operadoresUnicos = ['+', '*', '/', '='];
 
   currentInput: string = '';
   resultShown: boolean = false;
 
-  constructor(private historyService: HistoryService) {}
+  constructor(private historyService: HistoryService) { }
 
   onButtonClick(value: string) {
+
     if (this.resultShown) {
       this.currentInput = '';
       this.resultShown = false;
@@ -42,45 +26,49 @@ revisarOperadores() {
 
     if (value === '=') {
       this.calculateResult();
-    } else if (['+' , '*', '%', '/'].includes(value)) {
-      if (value === '-' && this.currentInput.endsWith('-')) {
-        this.currentInput = this.currentInput.slice(0, -1);
-      } else {
-        this.currentInput += `${value}`;
-        this.revisarOperadores();
-
-      }
-    } else if (value === '+/-') {
-      if (this.currentInput.startsWith('-')) {
-        this.currentInput = this.currentInput.substring(1);
-      } else {
-        this.currentInput = `-${this.currentInput}`;
-      }
-    } else if (value === '%') {
-      this.currentInput = (parseFloat(this.currentInput) * 0.01).toString();
-    } else if (value === 'AC') {
-      this.currentInput = '';
-    } else {
-      this.currentInput += value;
+      return;
     }
 
-}
+     if ((this.operadoresUnicos.includes(this.currentInput[this.currentInput.length - 1])
+      && this.operadoresUnicos.includes(value))) {
+      this.currentInput = this.currentInput.substring(0, this.currentInput.length - 1) + value;
+      return;
+    }
+     if (this.operadoresUnicos.includes(value)) {
+      this.currentInput += `${value}`;
+      return;
+    }
+     if (value === '%') {
+      this.currentInput = (parseFloat(this.currentInput) * 0.01).toString();
+      return;
+    }
+    if (value === 'AC') {
+      this.currentInput = '';
+      return;
+    }
+
+      this.currentInput += value;
+
+
+  }
 
 
   calculateResult() {
-    try {
-      const rawResult = evaluate(this.currentInput);
-      const result = parseFloat(rawResult).toLocaleString(undefined, {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2
-      });
-      const entry = `${this.currentInput} = ${result}`;
-      this.historyService.addToHistory(entry);
-      this.currentInput = result.toString();
-      this.resultShown = true;
-    } catch {
-      this.currentInput = 'Error';
-      this.resultShown = true;
+    if (this.currentInput.length > 1) {
+      try {
+        const rawResult = evaluate(this.currentInput);
+        const result = parseFloat(rawResult).toLocaleString(undefined, {
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 2
+        });
+        const entry = `${this.currentInput} = ${result}`;
+        this.historyService.addToHistory(entry);
+        this.currentInput = result.toString();
+        this.resultShown = true;
+      } catch {
+        this.currentInput = 'Error';
+        this.resultShown = true;
+      }
     }
   }
 }
